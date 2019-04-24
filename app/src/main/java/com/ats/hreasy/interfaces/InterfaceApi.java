6bc962a7619144720a09cc9933166d3192c6167c
Aaryatech <code@atsinfosoft.com>
2019-04-24 16:35:05 +0530

    //Anmol

--- app/src/main/java/com/ats/hreasy/interfaces/InterfaceApi.java
+++ app/src/main/java/com/ats/hreasy/interfaces/InterfaceApi.java
@@ -2,6 +2,7 @@ package com.ats.hreasy.interfaces;
 
 import com.ats.hreasy.model.DashboardCount;
 import com.ats.hreasy.model.LeaveApp;
+import com.ats.hreasy.model.LeaveEmployeeModel;
 import com.ats.hreasy.model.Login;
 
 import java.util.ArrayList;
@@ -15,15 +16,17 @@ import retrofit2.http.Query;
 public interface InterfaceApi {
 
     @POST("login")
-    Call<Login> doLogin(@Header("Authorization") String authHeader,@Query("username") String username, @Query("userPass") String userPass);
+    Call<Login> doLogin(@Header("Authorization") String authHeader, @Query("username") String username, @Query("userPass") String userPass);
 
 //    @Header("Authorization") String authHeader,
 
     @POST("getDashboardCount")
-    Call<DashboardCount> getDashboardCount(@Header("Authorization") String authHeader,@Query("empId") int empId);
+    Call<DashboardCount> getDashboardCount(@Header("Authorization") String authHeader, @Query("empId") int empId);
 
     @POST("getLeaveApplyListForAuth")
     Call<ArrayList<LeaveApp>> getLeaveApplyListForAuth(@Header("Authorization") String authHeader, @Query("empId") int empId, @Query("statusList") List<Integer> statusList, @Query("authTypeId") int authTypeId, @Query("currYrId") int currYrId);
 
+    @POST("getEmployeeListByEmpId")
+    Call<ArrayList<LeaveEmployeeModel>> getEmployeeListForLeave(@Header("Authorization") String authHeader, @Query("empId") int empId);
 
 }
